724c565e50fdb6a59ce8d8442052cd299c1c9e15
Deviprasad K P <111556892+Deviprasad0409@users.noreply.github.com>
2022-10-18 14:19:11 -0500

    Committing the solution for the question 3

--- Question3.py
+++ Question3.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt #Importing matplotlib module
+# Data to plot
+Programming_languages = 'Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++'
+Popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
+# exploding 1st slice
+explode = (0.1, 0, 0, 0,0,0)
+# Plot
+plt.pie(Popularity, explode=explode, labels=Programming_languages, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
 
+plt.axis('equal')
+plt.show()
